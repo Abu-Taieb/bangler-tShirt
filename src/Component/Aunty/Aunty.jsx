6acd4dc7b0a3e31eb935b9f54b6464ebaff91969
Abu-Taieb <taieb.contact.bd@gmail.com>
2023-04-08 00:19:39 +0600

    Props Drilling

--- src/Component/Aunty/Aunty.jsx
+++ src/Component/Aunty/Aunty.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Cusin from '../Cusin/Cusin';
 
 const Aunty = () => {
     return (
         <div>
             <h2>Aunty</h2>
+            <section className='flex'>
+                <Cusin>Rofiq</Cusin>
+                <Cusin>Sofiq</Cusin>
+            </section>
         </div>
     );
 };
---
+++ src/Component/Myself/Myself.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Special from '../Special/Special';
+import Friend from '../Friend/Friend';
+
+const Myself = () => {
+    return (
+        <div>
+            <h2>Myself</h2>
+            <Special></Special>
+            <Friend></Friend>
+        </div>
+    );
+};
+
+export default Myself;
